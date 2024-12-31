e482850857ae1ea0335e02b30022044dbe58cc9d
vale_hash <elekwachi50@gmail.com>
2024-12-31 12:30:01 +0100

    changed the export type in tailwind.css to use module type instead of common js added missing modules to pacakage.js changed the src/index.css to properly create the root and installed tailwaind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import animate from "tailwindcss-animate"
+
+
+export default {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -73,5 +76,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 }
